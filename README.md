40dae39f40df39f18f3446a29e7d632e6f6a4c52
Cristiano Betta <cbetta@paypal.com>
2014-05-03 19:22:56 +0100

    Adds basic code and instructions

---
+++ README.md
@@ -0,0 +1,24 @@
+# Using the PayPal REST API in Sinatra to make a PayPal payment
+
+## Technology
+
+This demo uses
+
+* Ruby 2.0 or higher
+* The Sinatra web framework
+* The PayPal Ruby REST SDK
+
+## Running the demo
+
+* Run `bundle` to install all dependencies
+* Run `ruby app.rb` to start the app
+* Visit http://127.0.0.1:4567/ in your browser
+* You will be redirected to PayPal
+* Login using the following credentials:
+  * Username: `us-customer@commercefactory.org`
+  * Password: `test1234`
+* Complete the payment instructions
+* You will receive a message that says "Payment completed"
+
+## Running the test
+
---
+++ app.rb
@@ -0,0 +1,49 @@
+require "sinatra"
+require "paypal-sdk-rest"
+
+include PayPal::SDK::REST
+
+enable :sessions
+
+get "/" do
+  payment = Payment.new({
+    intent: "sale",
+    payer: {
+      payment_method: "paypal"
+    },
+    redirect_urls: {
+      return_url: "http://127.0.0.1:4567/completed",
+      cancel_url: "http://127.0.0.1:4567/cancelled"
+    },
+    transactions: [
+      {
+        amount: {
+            total: "19.99",
+            currency: "EUR"
+          },
+        description: "A llama sweater"
+      }
+    ]
+  })
+
+  if payment.create
+    session[:payment_id] = payment.id
+    redirect payment.links.find {|link| link.method == "REDIRECT" }.href
+  else
+    "Handle the payment creation failure"
+  end
+end
+
+get "/cancelled" do
+  "The user has cancelled the payment"
+end
+
+get "/completed" do
+  payment = Payment.find(session[:payment_id])
+
+  if payment.execute(payer_id: params["PayerID"])
+    "Payment completed"
+  else
+    "Handle payment execution failure"
+  end
+end
